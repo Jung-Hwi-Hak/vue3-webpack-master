909d07cbe920f20414d1fc62b37454bc0fdd5f31
Jung-Hwi-Hak <gnlgkrdl@naver.com>
2021-08-11 16:40:56 +0900

    vue3-webpack-template-master-real

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+  created() {
+    console.log("Created!");
+  },
+  mounted() {
+    console.log("Mounted");
+  },
+};
+</script>
